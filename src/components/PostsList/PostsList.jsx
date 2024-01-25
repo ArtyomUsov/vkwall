5d544bc7674af79bc420d6c892e9ec1dacc85e4e
Usov_Artem <artemka063@gmail.com>
2024-01-25 13:56:44 +0400

    feat: add props transfer

--- src/components/PostsList/PostsList.jsx
+++ src/components/PostsList/PostsList.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Posts from "../Posts/Posts";
 import PostForm from "../PostForm/PostForm";
-import Header from "../Header/Header";
 
 const PostsList = () => {
+  const [newPost, setNewPost] = useState(""); // Состояние для хранения нового поста
+
+  const handleNewPost = (text) => {
+    setNewPost(text);
+    console.log(text); // Обновляем состояние нового поста
+  };
+
   return (
-    <div>
-      <Header/>
-      <Posts />
-      <PostForm />
+    <div className="post-container">
+      <PostForm onNewPost={handleNewPost} /> {/* Передаем функцию обратного вызова в PostForm */}
+      <Posts newPost={newPost} /> {/* Передаем новый пост в компонент Posts */}
     </div>
   );
 };
 
 export default PostsList;
+
