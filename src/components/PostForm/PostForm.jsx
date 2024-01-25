f893f6026be45a414265eebc17e7ef6425e236f5
Usov_Artem <artemka063@gmail.com>
2024-01-25 13:45:41 +0400

    feat: add  push posts logic

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
 
-const PostForm = () => {
-    return (
-        <div>
-            
-        </div>
+const PostForm = ({ onNewPost }) => {
+  const [textPost, setTextPost] = useState("");
+
+  const postText = () => {
+    fetch(
+      "https://b-base-fce24-default-rtdb.europe-west1.firebasedatabase.app/posts.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          text: textPost,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(
+      onNewPost(textPost),
+      setTextPost("")
     );
+  };
+
+
+  return (
+    <>
+      <form >
+        <input
+          type="text"
+          value={textPost}
+          placeholder="Введите ваше сообщение..."
+          onChange={(e) => setTextPost(e.target.value)}
+        />
+        <button onClick={(postText)} disabled={!textPost}>
+          отправить
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default PostForm;
