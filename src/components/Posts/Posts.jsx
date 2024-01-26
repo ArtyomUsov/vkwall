7ed61f988e897bc5aef447de7e6c1cc3e41a6e82
Usov_Artem <artemka063@gmail.com>
2024-01-26 17:29:15 +0400

    feat: implement loading of a random name, picture and current time to the post

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -11,7 +11,14 @@ const Posts = () => {
       .then((data) => {
         const result = [];
         for (const key in data) {
-          result.push({ id: key, text: data[key].text });
+          result.push({
+            id: key,
+            text: data[key].text,
+            userName: data[key].userName,
+            url: data[key].url,
+            likes: data[key].likes,
+            time: data[key].time,
+          });
         }
         setPosts(result);
       });
@@ -22,9 +29,19 @@ const Posts = () => {
   }, [posts]);
 
   return (
-    <div>
+    <div className="post-container">
       {posts.map((item) => (
-        <div key={item.id}>{item.text}</div>
+        <div key={item.id} className="post">
+          <div>
+            <img className="post-image" src={item.url} alt="avatar" />
+            <h6 className="time">{item.time}</h6>
+          </div>
+          <div className="relative">
+            <h3 className="post-author">{item.userName}</h3>
+            <p className="post-text">{item.text}</p>
+            <h5 className="likes">likes {item.likes}</h5>
+          </div>
+        </div>
       ))}
     </div>
   );
